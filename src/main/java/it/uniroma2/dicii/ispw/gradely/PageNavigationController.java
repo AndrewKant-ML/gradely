823c3e71e2634939b2d27447a3d41cbea4395d7d
andrea <andreacantarini2015@gmail.com>
2023-01-31 18:23:11 +0100

    (mar 31 gen 2023, 18:23:11, CET)

--- src/main/java/it/uniroma2/dicii/ispw/gradely/PageNavigationController.java
+++ src/main/java/it/uniroma2/dicii/ispw/gradely/PageNavigationController.java
@@ -32,6 +32,7 @@ public final class PageNavigationController {
             baseGraphicController.switchTo(
                     FXMLLoader.load(Objects.requireNonNull(MainApplication.class.getResource(pageName))));
         } catch (IOException e) {
+            showAlert(Alert.AlertType.ERROR, "Loading error", "");
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Loading error");
             alert.setContentText(String.format("Error while loading view. Error message: %s", e.getMessage()));
@@ -52,4 +53,20 @@ public final class PageNavigationController {
     }
 
     // TODO implement user switch
+
+    public void showAlert(Alert.AlertType alertType, String title, String message) {
+
+    }
+
+    /**
+     * Creates an alert message relative to a specific thrown exception
+     *
+     * @param alertType the type of Alert to be displayed
+     * @param title
+     * @param message
+     * @param e
+     */
+    public void showAlert(Alert.AlertType alertType, String title, String message, Exception e) {
+        showAlert(alertType, title, String.format(message + ". Error message: %s", e.getMessage()));
+    }
 }
--- src/main/java/it/uniroma2/dicii/ispw/gradely/enums/PendingEventTypeEnum.java
+++ src/main/java/it/uniroma2/dicii/ispw/gradely/enums/PendingEventTypeEnum.java
@@ -3,7 +3,7 @@ package it.uniroma2.dicii.ispw.gradely.enums;
 public enum PendingEventTypeEnum {
     EVENT_1("pending grade notification, system is waiting confirmation from student"),
     EVENT_2("The exam was marked accepted by the system due to lack of confirmation by the student"),
-    EVENT_3("The following exam grades are waitin for validity verification"),
+    EVENT_3("The following exam grades are waiting for validity verification"),
     EVENT_PROVA("prova evento"),
     EVENT_4("The exam verbale was protocolized by secretary"),
     EVENT_5("");
