3e0e5e0292c9f4f120109960e140774b9894acea
Giuseppe Valerio Cantone <giuseppe.valerio.cantone@uniroma2.it>
2023-02-05 11:33:03 +0100

    prova - before merging main to GVC - (Dom  5 Feb 2023 11:33:03 CET)

--- gitscripts/GVC/gitscript_commit_push_merge_main_to_GVC.sh
+++ gitscripts/GVC/gitscript_commit_push_merge_main_to_GVC.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 echo "(START OF SCRIPT) (START OF SCRIPT) (START OF SCRIPT) (START OF SCRIPT) (START OF SCRIPT) (START OF SCRIPT) (START OF SCRIPT) (START OF SCRIPT) (START OF SCRIPT) (START OF SCRIPT) (START OF SCRIPT) (START OF SCRIPT) (START OF SCRIPT) (START OF SCRIPT) (START OF SCRIPT) (START OF SCRIPT) (START OF SCRIPT) (START OF SCRIPT) (START OF SCRIPT) (START OF SCRIPT) (START OF SCRIPT) (START OF SCRIPT) (START OF SCRIPT)"
+echo "EXECUTING git switch GVC"
 git switch GVC
-git commit -am "$1  ($(date))"
+echo "EXECUTING git commit -am $1 - before merging main to GVC - ($(date))"
+git commit -am "$1 - before merging main to GVC - ($(date))"
+echo "EXECUTING git push -u origin GVC"
 git push -u origin GVC
+echo "EXECUTING git switch main"
 git switch main
-git commit -am "$1  ($(date))"
-git push -u origin main
-git switch GVC
+echo "EXECUTING git pull"
 git pull
+echo "EXECUTING git commit -am $1 - before merging main to GVC - ($(date))"
+git commit -am "$1 - before merging main to GVC - ($(date))"
+echo "EXECUTING git push -u origin main"
+git push -u origin main
+echo "EXECUTING git switch GVC"
+git switch GVC
+echo "EXECUTING git pull"
+git pull
+echo "EXECUTING git merge main"
 git merge main
+echo "EXECUTING git push -u origin GVC"
 git push -u origin GVC
 echo "(END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT)"
+
