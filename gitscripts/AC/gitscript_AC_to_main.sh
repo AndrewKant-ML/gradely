eb559da07a515e29caf73487619244f827108681
andrea <andreacantarini2015@gmail.com>
2023-02-05 19:02:11 +0100

    (dom 5 feb 2023, 19:02:11, CET)

--- gitscripts/AC/gitscript_AC_to_main.sh
+++ gitscripts/AC/gitscript_AC_to_main.sh
@@ -6,6 +6,8 @@ echo "->EXECUTING git commit -am \"$1  ($(date))\""
 git commit -am "$1  ($(date))"
 echo "->EXECUTING git push -u origin AC"
 git push -u origin AC
+echo "->EXECUTING git push -u copy AC"
+git push -u copy AC
 echo "->EXECUTING git switch main"
 git switch main
 echo "->EXECUTING git pull"
@@ -14,6 +16,8 @@ echo "->EXECUTING git merge AC"
 git merge AC
 echo "->EXECUTING git push -u origin main"
 git push -u origin main
+echo "->EXECUTING git push -u copy main"
+git push -u copy main
 echo "->EXECUTING git switch AC"
 git switch AC
 echo "(END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT)"
--- gitscripts/AC/gitscript_main.sh
+++ gitscripts/AC/gitscript_main.sh
@@ -6,6 +6,8 @@ echo "->EXECUTING git commit -am \"$1  ($(date))\""
 git commit -am "$1  ($(date))"
 echo "->EXECUTING git push -u origin main"
 git push -u origin main
+echo "->EXECUTING git push -u copy main"
+git push -u copy main
 echo "->EXECUTING git switch AC"
 git switch AC
 echo "(END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT)"
