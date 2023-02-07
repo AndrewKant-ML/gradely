28944e7eeff2fbf65c7f2f4fffcaac2c88fd49b0
Giuseppe Valerio Cantone <giuseppe.valerio.cantone@uniroma2.it>
2023-02-07 14:47:14 +0100

    - before merging GVC to main - (Mar  7 Feb 2023 14:47:14 CET)

--- gitscripts/AC/gitscript_main.sh
+++ gitscripts/AC/gitscript_main.sh
@@ -4,10 +4,10 @@ echo "----->EXECUTING git switch main"
 git switch main
 echo "----->EXECUTING git commit -am \"$1  ($(date))\""
 git commit -am "$1  ($(date))"
-echo "----->EXECUTING git push -u origin main"
-git push -u origin main
-echo "----->EXECUTING git push -u copy main"
-git push -u copy main
+echo "----->EXECUTING git push -u repoGVC main"
+git push -u repoGVC main
+echo "----->EXECUTING git push -u repoAC main"
+git push -u repoAC main
 echo "----->EXECUTING git switch AC"
 git switch AC
 echo "(END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT)"
--- gitscripts/main/gitscript_main_to_GVC.sh
+++ gitscripts/main/gitscript_main_to_GVC.sh
@@ -4,20 +4,20 @@ echo "----->EXECUTING git switch main"
 git switch main
 echo "----->EXECUTING git commit -am \"$1 - before merging main to GVC - ($(date))\""
 git commit -am "$1 - before merging main to GVC - ($(date))"
-echo "----->EXECUTING git push -u origin main"
-git push -u origin main
-echo "----->EXECUTING git push -u copy main"
-git push -u copy main
+echo "----->EXECUTING git push -u repoGVC main"
+git push -u repoGVC main
+echo "----->EXECUTING git push -u repoAC main"
+git push -u repoAC main
 echo "----->EXECUTING git switch GVC"
 git switch GVC
 echo "----->EXECUTING git pull"
-git pull origin GVC
+git pull repoGVC GVC
 echo "----->EXECUTING git merge main"
 git merge main
-echo "----->EXECUTING git push -u origin GVC"
-git push -u origin GVC
-echo "----->EXECUTING git push -u copy GVC"
-git push -u copy GVC
+echo "----->EXECUTING git push -u repoGVC GVC"
+git push -u repoGVC GVC
+echo "----->EXECUTING git push -u repoAC GVC"
+git push -u repoAC GVC
 echo "----->EXECUTING git switch main"
 git switch main
 echo "(END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT) (END OF SCRIPT)"
