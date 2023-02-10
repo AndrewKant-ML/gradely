5f74d7ab47c8ffd327838a37b8f74ae1ec70249c
andrea <andreacantarini2015@gmail.com>
2023-02-10 12:27:52 +0100

    (ven 10 feb 2023, 12:27:52, CET)

---
+++ src/main/java/it/uniroma2/dicii/ispw/gradely/use_cases/insert_students_grades/graphic_controllers/professor/InsertResultsAndGradesGraphicController.java
@@ -0,0 +1,18 @@
+package it.uniroma2.dicii.ispw.gradely.use_cases.insert_students_grades.graphic_controllers.professor;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class InsertResultsAndGradesGraphicController implements Initializable {
+    @FXML
+    public TableView enrollmentsTable;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
