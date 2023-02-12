919f84ccb62a0ef10cbf6248126f39cb94f7c93c
GVCUTV <giuseppevalerio.cantone@students.uniroma2.eu>
2023-02-13 00:37:42 +0100

    (Mon Feb 13 00:37:42 CET 2023)

--- src/main/java/it/uniroma2/dicii/ispw/gradely/beans_general/StudentBean.java
+++ src/main/java/it/uniroma2/dicii/ispw/gradely/beans_general/StudentBean.java
@@ -4,8 +4,15 @@ public class StudentBean {
 
     private String codiceFiscale;
 
+    private String matricola;
+
     public StudentBean(String codiceFiscale) {
         this.codiceFiscale = codiceFiscale;
+    }
+
+    public StudentBean(String codiceFiscale, String matricola) {
+        this.codiceFiscale = codiceFiscale;
+        this.matricola = matricola;
     }
 
     public String getCodiceFiscale() {
@@ -14,5 +21,13 @@ public class StudentBean {
 
     public void setCodiceFiscale(String codiceFiscale) {
         this.codiceFiscale = codiceFiscale;
+    }
+
+    public String getMatricola() {
+        return matricola;
+    }
+
+    public void setMatricola(String matricola) {
+        this.matricola = matricola;
     }
 }
